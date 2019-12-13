68747ff3cb6e256012671cc207254f26264c624d
Charles Serrato <serrato@gmail.com>
2019-12-13 17:02:21 -0300

    testes

--- tests/Feature/ErrorTest.php
+++ tests/Feature/ErrorTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class ErrorTest extends TestCase
@@ -12,21 +13,36 @@ class ErrorTest extends TestCase
     use DatabaseMigrations;
 
     protected $token;
+    protected $user;
 
     protected function setUp() : void
     {
         parent::setUp();
         $userFake = factory(\App\User::class)->create();
-        $user = [
+        $this->user = [
             'email' => $userFake->email,
             'password' => '123456'
         ];
 
-        $token = $this->post(route('api.login'), $user)->getContent();
+        $token = $this->post(route('api.login'), $this->user)->getContent();
         $this->token = json_decode($token)->token;
     }
 
+    // public function testDatabase()
+    // {
+    //     // Make call to application...
+    //     $u = new \App\User();
+    //     $u->name = "Nome";
+    //     $u->email = "teste@teste.com";
+    //     $u->admin = '1';
+    //     $u->password = '$2y$10$YPDvYV/jAuxxJlWUPaOBT.2qBbPpspLpfqNqrxVS6P.FrSh/gY/by';
+    //     $u->remember_token = Str::random(10);
+    //     $u->save();
 
+    //     $this->assertDatabaseHas('users', [
+    //         'email' => $u->email
+    //     ]);
+    // }
 
     public function testRequisicaoValida():void
     {
@@ -54,6 +70,5 @@ class ErrorTest extends TestCase
         ]);
 
         $response->assertStatus(400);
-
     }
 }
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -10,6 +10,22 @@ use Tests\TestCase;
 class UserTest extends TestCase
 {
     use DatabaseMigrations;
+    
+    protected $token;
+    protected $user;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $userFake = factory(\App\User::class)->create();
+        $this->user = [
+            'email' => $userFake->email,
+            'password' => '123456'
+        ];
+
+        $token = $this->post(route('api.login'), $this->user)->getContent();
+        $this->token = json_decode($token)->token;
+    }
 
     public function testIsUser():void
     {
@@ -18,4 +34,39 @@ class UserTest extends TestCase
     }
 
 
+/*
+$u = new \App\User();
+    //     $u->name = "Nome";
+    //     $u->email = "teste@teste.com";
+    //     $u->admin = '1';
+    //     $u->password = '$2y$10$YPDvYV/jAuxxJlWUPaOBT.2qBbPpspLpfqNqrxVS6P.FrSh/gY/by';
+    //     $u->remember_token = Str::random(10);
+    //     $u->save();
+*/
+
+
+    public function testRequisicaoValida():void
+    {
+        $response = $this->withHeaders(['Authorization' => 'bearer'.$this->token])
+                            ->post(route('user.save'), [
+                                'name' => 'Nome',
+                                'email' => 'teste@teste.com',
+                                'admin' => '1',
+                                'password' => '123456'
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function testRequisicaoInvalida():void
+    {
+        $response = $this->withHeaders(['Authorization' => 'bearer'.$this->token])
+                            ->post(route('user.save'), [
+                                'name' => 'Nome',
+                                'admin' => '1',
+                                'password' => '123456'
+        ]);
+
+        $response->assertStatus(400);
+    }
 }
